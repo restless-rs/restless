c03c021b22a0f70d12ec2586393e413914b20300
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-05-10 00:17:52 +0300

    Make `RouteHandler` methods return `Self`

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -4,11 +4,16 @@ use restly::router::RouteHandler;
 fn main() {
     let port = 3000;
     let mut app = App::new();
-    app.listen(port, || {
-        println!("Bind at {port} port")
-    });
 
     app.get("/", || {
         println!("Handled / path")
+    }).get("/item/:u64", || {
+        println!("Give some item")
+    }).post("/item/name:str&value:u32", || {
+        println!("Got some item")
+    });
+
+    app.listen(port, || {
+        println!("Bind at {port} port")
     });
 }
