7ea1b403091b284dc18ed7bd70b7bd4d02aedfab
gregor <e@tokarev.work>
2023-05-09 19:04:07 +0300

    add path

--- restly/src/route.rs
+++ restly/src/route.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 enum PathItemType {
     Static,
     Dynamic,
@@ -8,7 +10,53 @@ struct PathItem<'a> {
     value: &'a str,
 }
 
+impl PathItem<'_> {
+    pub fn new(value: &str, r#type: PathItemType) -> PathItem {
+        PathItem {
+            value,
+            r#type,
+        }
+    }
+}
+
 pub struct Route<'a> {
     paths: Vec<PathItem<'a>>,
-    handler: Box<dyn Fn()>,
+    handler: fn(),
+}
+
+impl Route<'_> {
+    pub fn new(path: &str, handler: fn()) -> Route {
+        Route {
+            paths: Route::parse_path(path),
+            handler
+        }
+    }
+
+    fn parse_path(path: &str) -> Vec<PathItem> {
+        if !path.starts_with("/") {
+            panic!("Path {} should starts with /", path)
+        };
+
+        path.split("/")
+            .map(|path_part| {
+                let path_type = if path_part.starts_with(":") {
+                    PathItemType::Dynamic
+                } else {
+                    PathItemType::Static
+                };
+
+                PathItem::new(path_part, path_type)
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::route::{PathItem, PathItemType, Route};
+
+    #[test]
+    fn create_route() {
+        let route = Route::new("/", || {});
+    }
 }
