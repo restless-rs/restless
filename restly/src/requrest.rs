31360abe03ab761f8d7587dcf5763a278e05ed1a
gregor <e@tokarev.work>
2023-05-10 13:12:15 +0300

    fix `Req` struct

--- restly/src/requrest.rs
+++ restly/src/requrest.rs
@@ -1,47 +1,20 @@
-enum ReqMethod {
-    GET,
-    POST,
-    PATCH,
-    PUT,
-    DELETE,
-}
+use tokio::io;
 
-impl ReqMethod {
-    pub fn from_string(raw_method: &str) -> Option<ReqMethod> {
-        let raw_method = raw_method.to_uppercase();
-
-        if raw_method == "GET" {
-            Some(ReqMethod::GET)
-        } else if raw_method == "POST" {
-            Some(ReqMethod::POST)
-        } else if raw_method == "PATCH" {
-            Some(ReqMethod::PATCH)
-        } else if raw_method == "DELETE" {
-            Some(ReqMethod::DELETE)
-        } else if raw_method == "PUT" {
-            Some(ReqMethod::PUT)
-        } else {
-            None
-        }
-    }
-}
-
+#[derive(Default)]
 pub struct Req<'a> {
     body: Option<&'a str>,
     path: &'a str,
-    method: ReqMethod,
+    method: &'a str,
 }
 
-impl Req<'_> {
-    pub fn new(raw_req: String) {
+impl<'a> Req<'a> {
+    pub fn new(raw_req: String) -> Result<Req<'a>, io::Error> {
         let mut lines = raw_req.lines();
-        let req = Req {
-            body: None,
-            path: "",
-            method: ReqMethod::GET,
-        };
+        let req = Req::default();
 
         let main_info = lines.next();
+
+        Ok(req)
     }
 
     fn parse_main(line: &str) {}
